f9ecfbbf91d7f3775a1c062ba75971fcdfb68a0f
shreyag.coder <2002sgupta@gmail.com>
2026-01-18 10:52:58 +0530

    Assignment 1

---
+++ main.py
@@ -0,0 +1,19 @@
+#Task 1
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+
+print("Addition: ", add)
+print("Subtraction: ", sub)
+print("Multiplication: ", mul)
+print("Division: ", div)
+
+#Task 2
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print(f"Hello, {first_name}! Welcome to {last_name} the Python program.")
